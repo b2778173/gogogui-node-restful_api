3819574d0d0fa9fc3ffb51b4e9f864858ee102a7
Ryan Tsai <bp4m4h94@gmail.com>
2021-05-07 21:00:04 +0800

    feat: 新增 api rmWatchlist
    刪除watchlist

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -17,13 +17,11 @@ router.get('/getSymbolList', async (req, res) => {
     }
 });
 
-// symbol lookup
 router.post('/addWatchlist', async (req, res) => {
     try {
         // validation
         const { error } = validate(req.body);
         if (error) return res.status(400).send({ message: error.details[0].message });
-
         const { symbol } = req.body;
         // eslint-disable-next-line no-underscore-dangle
         const _id = symbol;
@@ -33,6 +31,25 @@ router.post('/addWatchlist', async (req, res) => {
         });
         await addedWatchlist.save();
         res.send({ result: addedWatchlist });
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+});
+
+router.post('/rmWatchlist', async (req, res) => {
+    try {
+        // validation
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send({ message: error.details[0].message });
+        const { symbol } = req.body;
+        // eslint-disable-next-line no-underscore-dangle
+        const _id = symbol;
+
+        const removedWatchlist = new Watchlist({
+            _id
+        });
+        await removedWatchlist.remove();
+        res.send({ result: removedWatchlist });
     } catch (e) {
         res.status(400).send(e.message);
     }
@@ -49,4 +66,3 @@ router.get('/getAllWatchlist', async (req, res) => {
 });
 
 module.exports = router;
-
